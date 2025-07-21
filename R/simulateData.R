af2eeb40cbfdea05c2de25443f409cdf9db1fb89
Kenneth Nieser <knieser1@gmail.com>
2025-07-21 10:54:20 -0700

    cleaned documentation and added example

--- R/simulateData.R
+++ R/simulateData.R
@@ -1,14 +1,51 @@
 #' Simulate data
 #' @description
 #' This function simulates some data.
+#' @param n.entity total number of entities to simulate
+#' @param avg.n average number of observations per entity; entity sample sizes are simulated from a Poisson distribution with mean avg.n
+#' @param tau parameters for distribution of random intercepts. For binary observations, input a vector of length 2 where the 1st entry is the overall probability of the outcome on the log-odds scale and the 2nd entry is the between-entity standard deviation.
+#' For continuous observations, input a vector of length 2 where the 1st entry is the between-entity standard deviation and the 2nd entry is the within-entity (or residual) standard deviation.
+#' @param theta regression coefficient for covariate added to the linear predictor; default is 0.
+#' @param type type of data to simulate. Valid options include: 'binary' (default) and 'normal'.
 #' @author Kenneth Nieser (nieser@stanford.edu)
-#' @references None
 #' @examples
-#' # TBD
+#' # number of accountable entities
+#' n.entity = 100
+#'
+#' # average number of patients or cases per accountable entity
+#' avg.n = 50
+#'
+#' # marginal probability of the outcome
+#' p = 0.1
+#'
+#' # approximate reliability for entity with an average number of patients
+#' r = 0.6
+#'
+#' # implied between-entity variance
+#' var.btwn = r / (1 - r) * (1/(avg.n * p * (1 - p)))
+#'
+#' mu = log(p / (1-p))
+#' tau = c(mu, sqrt(var.btwn))
+#'
+#' # parameter for risk-adjustment model (i.e., coefficient for x1)
+#' theta = log(1.5)
+#'
+#' df <- simulateData(n.entity = n.entity, avg.n = avg.n, tau = tau, theta = theta)
+#' head(df)
+#'
 #' @importFrom stats aov
 #' @export
 
-simulateData <- function(n.entity, avg.n, tau = tau, theta = theta, type = 'binary'){
+simulateData <- function(n.entity, avg.n, tau, theta = 0, type = 'binary'){
+
+  if (!is.numeric(n.entity)){stop('n.entity must be a positive integer.')}
+  if (floor(n.entity) != n.entity){stop('n.entity must be a positive integer.')}
+  if (n.entity <= 0){stop('n.entity must be a positive integer.')}
+  if (!is.numeric(avg.n)){stop('avg.n must be a positive integer.')}
+  if (floor(avg.n) != avg.n){stop('avg.n must be a positive integer.')}
+  if (avg.n <= 0){stop('avg.n must be a positive integer.')}
+  if (length(tau) != 2){stop('tau must be a vector of length 2.')}
+  if (type != 'binary' &  type != 'normal'){stop('Valid options for data type include binary and normal.')}
 
   n = rpois(n.entity, avg.n)
   total.n = sum(n)
