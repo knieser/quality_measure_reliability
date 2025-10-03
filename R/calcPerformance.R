5ada10be01b2a936fc09e606c3030a3deb75e6fa
Kenneth Nieser <knieser1@gmail.com>
2025-10-03 10:56:25 -0700

    retired separate profiling_analysis function

--- R/calcPerformance.R
+++ R/calcPerformance.R
@@ -258,10 +258,45 @@ calcPerformance <- function(df = NULL, model = NULL, entity = "entity", y = "y",
         intercept.OR.lwr = exp(m.re.intercept$est - z*m.re.intercept$sd),
         intercept.OR.upr = exp(m.re.intercept$est + z*m.re.intercept$sd)
       )
+
+    rate.summary = rbind(summary(perf.results$p),
+                         summary(perf.results$rs.oe),
+                         summary(perf.results$rs.pe),
+                         summary(perf.results$rs.direct)
+    )
+    perf.summary = cbind(Method = c('Unadjusted', 'OE standardized', 'PE standardized', 'Direct standardized'), round(rate.summary, 3))
+    perf.summary = as.data.frame(perf.summary)
+    category.table = table(perf.results$category.oe, perf.results$category.pe)
+    corr.oe.randint = stats::cor(perf.results$intercept.OR, perf.results$oe)
+    corr.pe.randint = stats::cor(perf.results$intercept.OR, perf.results$pe)
+    icc.oe.randint = psych::ICC(perf.results[,c('intercept.OR', 'oe')])
+    icc.pe.randint = psych::ICC(perf.results[,c('intercept.OR', 'pe')])
+
   }
+
   perf.results$intercept.sig = as.factor(ifelse(perf.results$intercept.OR.lwr > 1 | perf.results$intercept.OR.upr < 1, 1, 0))
 
-  results = list(call = cl, df = df, model = model, fit = fit, marg.p = marg.p, perf.results = perf.results)
+  if(risk.adj==0){
+    results = list(call = cl,
+                   df = df,
+                   model = model,
+                   fit = fit,
+                   marg.p = marg.p,
+                   perf.results = perf.results)
+  } else{
+    results = list(call = cl,
+                   df = df.perf,
+                   model = model,
+                   fit = fit,
+                   marg.p = marg.p,
+                   perf.results = perf.results,
+                   perf.summary = perf.summary,
+                   category.table = category.table,
+                   corr.oe.randint = corr.oe.randint,
+                   corr.pe.randint = corr.pe.randint,
+                   icc.oe.randint = icc.oe.randint,
+                   icc.pe.randint = icc.pe.randint)
+  }
 
   return(results)
 }
